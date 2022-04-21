59c7be4f1e1ed0c1a22710f8eddd66e3cfd3535d
Dhruvik Kava <dhruvik.kava@sparsh1.com>
2022-04-21 10:22:47 +0530

    committed home page

--- assets/js/general.js
+++ assets/js/general.js
@@ -13,25 +13,37 @@ $('.slider-slick').slick({
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
+          dots: true,
+          variableWidth: false
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 2,
+          variableWidth: false
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          variableWidth: false
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
+
+$(document).ready(function(){
+  $(".fa-bars").click(function(){
+    document.body.style.overflow = document.body.style.overflow == 'hidden' ? 'auto' : 'hidden'
+  });   
+  $(".overlay").click(function(){
+    document.body.style.overflow = 'auto'
+  });
+  $(".fa-times").click(function(){
+    document.body.style.overflow = 'auto'
+  })
+})
